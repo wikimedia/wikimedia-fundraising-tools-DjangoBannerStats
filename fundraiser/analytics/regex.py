35c45f309a4d1f24dde17a9d8f449d92d5648cea
Peter Gehres <pgehres@wikimedia.org>
2012-05-09 22:02:41 -0700

    Adding a bunch of regexes for various purposes.

--- fundraiser/analytics/regex.py
+++ fundraiser/analytics/regex.py
@@ -61,14 +61,109 @@ queryparts = re.compile(
     """, re.VERBOSE
 )
 
+# some quick regexes to pare down the parse list and not try our best to parse everything
+landingpages_ignore = [
+    re.compile(
+        r"""
+            # ignore calls to Special:LandingCheck. They 302 and then result in a proper call to the landing page
+            # also ignore Special:ContributionTracking and the MediaWiki namespace
+            (http|https)
+            ://wikimediafoundation.org/
+            (
+                wiki/
+              | w/index.php\?title=
+            )
+            (
+                Special:Landingcheck
+              | Special:ContributionTracking
+              | Special:RecentChanges
+              | MediaWiki:
+              | File:
+              | Talk:
+            )
+        """, re.VERBOSE | re.IGNORECASE
+    ),
+    re.compile(
+        r"""
+            # ignore calls to the api as well as the favicon and the MediaWiki namespace
+            (http|https)
+            ://wikimediafoundation.org/
+            (
+                upload/
+              | w/
+                (
+                    skins-
+                  | api.php
+                  | opensearch_desc.php
+                  | index.php\?search=
+                  | extensions/
+                )
+              | favicon.ico
+              | tracker/bannerImpression.php
+            )
+        """, re.VERBOSE | re.IGNORECASE
+    ),
+    re.compile(
+        r"""
+            # ignore the ToU for now
+            (http|https)
+            ://wikimediafoundation.org/
+            (
+                wiki/
+              | w/index.php\?title=
+            )
+            (
+                Terms_of_Use
+              | New_Terms_of_use
+              | New%20Terms%20of%20use
+              | Feedback_privacy_statement
+              | Home
+              | Main_Page
+              | Donate/Benefactor
+              | Donate/Stories
+              | Donate/Thank_You
+              | Donate/Transparency
+
+            )
+        """, re.VERBOSE | re.IGNORECASE
+    ),
+]
 
 landingpages = [
     re.compile(
         r"""
+            # match all of the landing page patterns on wmfwiki
             (http|https)
             ://
-            wikimediafoundation.org/w/index.php?title=
-            (Special:LandingCheck|index.php)
+            (?P<sitename>wikimediafoundation.org)/
+            (wiki/|w/index.php\?title=)
+            (?P<landingpage>
+                (
+                    L11                 # landing page naming scheme for 2011
+                  | L12                 # landing page naming scheme for 2012
+                  | L2011               # potential landing page naming scheme for 2011
+                  | L2012               # potential landing page naming scheme for 2012
+                  | WMF                 # eg WMFJA085
+                  | Donate              # old forms, keeping so that we can possibly redirect them all
+                  | Support_Wikipedia   # old forms
+                )
+                (?:(?!\?|&)[\S])*         # this will give us the landing page up to the next ? or &
+            )
+        """, re.VERBOSE | re.IGNORECASE
+    ),
+    re.compile(
+        r"""
+            (http|https)
+            ://
+            (?P<sitename>donate.wikimedia.org)/
+            (
+                wiki/
+              | w/index.php\?title=
+            )
+            (
+                Special:FundraiserLandingPage
+#              | Special:FundraiserRedirector # these 302 and should result in a valid call to S:FLP
+            )
         """, re.VERBOSE | re.IGNORECASE
     )
 ]
